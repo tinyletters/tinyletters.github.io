eaf4f776413ab106e4d14cf654b6cb90ab3b115c
larakoseff <larakoseff@gmail.com>
2024-08-23 14:29:37 +0200

    replace vite text

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tiny letters</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        Tiny letters for mothers is an artistic research project that aims to create a global network of data stories about the first 40 days / 6 weeks postpartum. The title tiny letters is borne of Dee Marco’s 40+day postpartum writing after the birth of her third child in 2023. Her candid and sometimes raw and scary recollections of the time set off a series of conversations that led to the best kind of working collaboration – one where mums work together. Dee and creative developer Lara Koseff embark on this journey of learning about global experiences of mothers by creating a data story through tiny letters from one mum to another, from one geography to another, from one illogical moment to another. 
         </p>
       </div>
       <p className="read-the-docs">
