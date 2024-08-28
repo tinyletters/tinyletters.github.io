d68d41ab8f6b69deb73bb15a9915f369d6ce92b2
larakoseff <larakoseff@gmail.com>
2024-08-28 09:42:30 +0200

    Add header and intro para text and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import { Circles } from './Circle'
+import { useState } from "react";
+import "./App.css";
+import { Circles } from "./Circle";
+import Header from "./Header";
 
 function App() {
-
   return (
     <>
-      <div>
+      <Header />
+      <div className="intro--card">
+        <div className="about">
+          <h2>about</h2>
+          <p>
+            To contribute to our first data story{" "}
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdF396c_2UL8Z_zjiszvVsEdRb5QHnJxeEhrVlxpgoEiidNig/viewform" target="_blank">
+              fill out this form
+            </a>
+          </p>
+        </div>
+        <div className="card">
+          <p>
+            Tiny letters for mothers is an artistic research project that aims
+            to create a global network of data stories about the first 40 days /
+            6 weeks postpartum. The title tiny letters is borne of Dee Marco’s
+            40+day postpartum writing after the birth of her third child in
+            2023. Her candid and sometimes raw and scary recollections of the
+            time set off a series of conversations that led to the best kind of
+            working collaboration – one where mums work together. Dee and
+            creative developer Lara Koseff embark on this journey of learning
+            about global experiences of mothers by creating a data story through
+            tiny letters from one mum to another, from one geography to another,
+            from one illogical moment to another.
+          </p>
+        </div>
       </div>
-      <h1>Tiny letters</h1>
-      <div className="card">
-        <p>
-        Tiny letters for mothers is an artistic research project that aims to create a global network of data stories about the first 40 days / 6 weeks postpartum. The title tiny letters is borne of Dee Marco’s 40+day postpartum writing after the birth of her third child in 2023. Her candid and sometimes raw and scary recollections of the time set off a series of conversations that led to the best kind of working collaboration – one where mums work together. Dee and creative developer Lara Koseff embark on this journey of learning about global experiences of mothers by creating a data story through tiny letters from one mum to another, from one geography to another, from one illogical moment to another. 
-        </p>
-        <Circles/>
+      <div>
+        <Circles />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./App.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header--box">
+        <div className="header--title">tiny letters for mothers</div>
+        <img src="../images/logo.svg" className="header--logo" alt="logo" />
+      </div>
+    </>
+  );
+}
